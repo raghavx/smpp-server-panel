148725e660f26f4482400b21320ea1ab55319e0e
Raghvendra Sahu <rx.sahu@gmail.com>
2017-03-24 01:58:29 +0530

    initial commit

---
+++ src/main/java/com/raghavx/smpp/SmppServerPanelApplication.java
@@ -0,0 +1,12 @@
+package com.raghavx.smpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmppServerPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmppServerPanelApplication.class, args);
+	}
+}
